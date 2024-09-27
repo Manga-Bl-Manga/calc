07a7a9112ce5508e2937f24daa3d1fd0f64a781e
codeGaso <157953860+Manga-Bl-Manga@users.noreply.github.com>
2024-09-27 12:44:49 +0300

    :)

--- js.js
+++ js.js
@@ -16,9 +16,49 @@ function divide(a, b){
     return (a / b);
 }
 
+let currentShowed = '';
+let textbar = document.querySelector(".result-bar")
+textbar.textContent = currentShowed;
 let firstNum = 0;
 let secondNum = 0;
 let operator = '';
+
+let addButt = document.querySelector('.addButt')
+addButt.addEventListener("click", () => {
+    operator = '+';
+    if (currentShowed !== ''){
+        firstNum = parseInt(currentShowed);
+    }
+    currentShowed = ''
+})
+
+let minusButt = document.querySelector('.minusButt')
+minusButt.addEventListener("click", () => {
+    operator = '-';
+    if (currentShowed !== ''){
+        firstNum = parseInt(currentShowed);
+    }
+    currentShowed = ''
+})
+
+let multiButt = document.querySelector('.multiButt')
+multiButt.addEventListener("click", () => {
+    operator = '*';
+    if (currentShowed !== ''){
+        firstNum = parseInt(currentShowed);
+    }
+    currentShowed = ''
+})
+
+let divideButt = document.querySelector('.divideButt')
+divideButt.addEventListener("click", () => {
+    operator = '/';
+    if (currentShowed !== ''){
+        firstNum = parseInt(currentShowed);
+    }
+    currentShowed = ''
+})
+
 
 function operate(num1, num2, op){
     op = operator;
@@ -41,13 +81,50 @@ function operate(num1, num2, op){
         return divide(firstNum,secondNum);
     }
 
-    else return 'erorr, not a valid operator'
+    else return currentShowed;
 }
 
 let numbers = document.querySelector(".numbers")
+
 for (let i = 9; i >= 0; i--){
     let number = document.createElement("button");
     number.textContent = i;
     number.className = "number";
+    number.addEventListener("click", () => {
+        currentShowed += i;
+        textbar.textContent = currentShowed;
+        console.log(currentShowed);
+        console.log("i was clicke" + i);
+    })
     numbers.appendChild(number);
 }
+
+let clearButt = document.createElement("button")
+clearButt.className = "number";
+clearButt.textContent = "C";
+
+let equalButt = document.createElement("button")
+equalButt.className = "number";
+equalButt.textContent = "=";
+
+numbers.appendChild(clearButt)
+numbers.appendChild(equalButt)
+
+
+
+equalButt.addEventListener('click', () => {
+    secondNum = parseInt(currentShowed);
+    let result = operate(firstNum, secondNum, operator);
+    currentShowed = result.toString();
+    firstNum = result;
+    textbar.textContent = currentShowed;
+    console.log( 'current showed is: ' + currentShowed + ', firstNum is: ' + firstNum + ', secondNum is' + secondNum + operator)
+    if(operator !== '')currentShowed = '';
+    operator = ''; 
+})
+
+clearButt.addEventListener('click', () => {
+    currentShowed = ''
+    textbar.textContent = currentShowed;
+    console.log(currentShowed + " " + textbar.textContent)
+})
